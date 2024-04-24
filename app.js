a1bafd81894ca1a7f2d31e8ec024c0ccefe80db4
okashabib-oh <okasha.qavitech786@gmail.com>
2024-04-25 01:29:31 +0500

    date and time app

--- app.js
+++ app.js
@@ -17,17 +17,3 @@ let wholeDateDiv = document.getElementById('wholeDate')
 
 dateDiv.innerHTML = myTime;
 wholeDateDiv.innerHTML = wholeDate;
-// if(myTime > 3){
-//     document.getElementById('greeting').innerHTML += "Good Afternoon";
-// }else{
-//     document.getElementById('greeting').innerHTML += "Good Night";
-
-// }
-
-if(myTime < "11:59 AM"){
-    document.getElementById("greeting").innerHTML += "Good Morning";
-}else if(myTime >= "12:00 PM" && myTime < "18:00 PM"){
-    document.getElementById("greeting").innerHTML += "Good Afternoon"
-}else if(myTime >= "18:00 PM" && myTime < "24:00 PM"){
-    document.getElementById("greeting").innerHTML += "Good Evening"
-}
